49df8d7c498ba0ce74c48e1fe665431932dff392
Sarah Schulte <sarah.schulte@samsara.com>
2021-12-27 09:12:39 -0800

    fix websocket proxy

--- viz/index.js
+++ viz/index.js
@@ -20,13 +20,23 @@ const proxy = createProxyMiddleware({
   changeOrigin: true,
 });
 
+const wsProxy = createProxyMiddleware("/stream", {
+  target: API_SERVICE_URL,
+  changeOrigin: true,
+  logLevel: "debug",
+  ws: true,
+});
+
+app.use(wsProxy);
+
 app.use("/viz", express.static("src"));
-app.use("/stream", proxy);
 app.use("/", proxy);
 
 open(`http://${HOST}:${PORT}/viz/index.html`);
 
 // Start the Proxy
-app.listen(PORT, HOST, () => {
-  console.log(`Starting Proxy at ${HOST}:${PORT}`);
-});
+app
+  .listen(PORT, HOST, () => {
+    console.log(`Starting Proxy at ${HOST}:${PORT}`);
+  })
+  .on("upgrade", wsProxy.upgrade); // <-- subscribe to http 'upgrade';
