e5867fdd5dae3eb30253f6886c94cdc406ab94ef
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2022-11-12 05:14:16 -0800

    casino: finish updating packages

--- casino/bootstrap/src/main.rs
+++ casino/bootstrap/src/main.rs
@@ -1,6 +1,6 @@
 use std::path::{Path, PathBuf};
 
-use clap::{App, Arg};
+use clap::Parser;
 use reqwest::{Client, Url};
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
@@ -10,59 +10,48 @@ lazy_static::lazy_static! {
     static ref BINARY_URL: Url = "https://www.dropbox.com/s/x0vxr1kvpadugga/collector.exe".parse().unwrap();
 }
 
+#[derive(Parser)]
+struct Args {
+    /// Path to configuration file
+    #[arg(env, default_value = "./config.yaml")]
+    config_path: PathBuf,
+    /// Path to write the output binary to
+    #[arg(short, long, env, default_value = "./collector.exe")]
+    out_file: PathBuf,
+    /// If provided, don't actually run the binary, just prepare it
+    #[arg(short, long, env, action = clap::ArgAction::SetTrue)]
+    dry_run: bool,
+    /// Level to log at
+    #[arg(short, long, env, default_value = "info")]
+    log_level: log::LevelFilter,
+}
+
 #[tokio::main]
 async fn main() {
-    let args = App::new("bootstrap")
-        .arg(logging::build_arg())
-        .arg(
-            Arg::with_name("config_path")
-                .index(1)
-                .env("CONFIG_PATH")
-                .help("Path to configuration file")
-                .takes_value(true)
-                .default_value("./config.yaml"),
-        )
-        .arg(
-            Arg::with_name("out_file")
-                .short("-o")
-                .long("--out-file")
-                .help("Path to write the output binary to")
-                .takes_value(true)
-                .default_value("./collector.exe"),
-        )
-        .arg(
-            Arg::with_name("dry_run")
-                .short("-d")
-                .long("--dry-run")
-                .takes_value(false)
-                .help("If provided, don't actually run the binary, just prepare it")
-        )
-        .get_matches();
+    // TODO
+    let args = Args::parse();
 
-    let log_level: String = args.value_of(logging::LEVEL_FLAG_NAME).unwrap().to_string();
-    logging::init_str(&log_level);
+    // let log_level: String = args.value_of(logging::LEVEL_FLAG_NAME).unwrap().to_string();
+    logging::init(args.log_level);
 
-    let config_path: PathBuf = args.value_of("config_path").unwrap().parse().unwrap();
-    if !config_path.exists() {
+    // let config_path: PathBuf = args.value_of("config_path").unwrap().parse().unwrap();
+    if !args.config_path.exists() {
         log::warn!(
             "Config file {} doesn't exist",
-            config_path.to_str().unwrap()
+            args.config_path.to_str().unwrap()
         );
 
         std::process::exit(1);
     }
 
-    let out_path: PathBuf = args.value_of("out_file").unwrap().parse().unwrap();
-    if !out_path.exists() {
-        log::info!("Downloading binary to {}...", out_path.as_os_str().to_str().unwrap());
-        download_file(&out_path).await;
+    if !args.out_file.exists() {
+        log::info!("Downloading binary to {}...", args.out_file.as_os_str().to_str().unwrap());
+        download_file(&args.out_file).await;
     }
 
-    let is_dry_run = args.is_present("dry_run");
-
-    if !is_dry_run {
-        Command::new(out_path.as_os_str())
-            .arg(config_path.as_os_str())
+    if !args.dry_run {
+        Command::new(args.out_file.as_os_str())
+            .arg(args.config_path.as_os_str())
             .spawn()
             .unwrap()
             .wait()
--- casino/logging/src/lib.rs
+++ casino/logging/src/lib.rs
@@ -1,19 +1,7 @@
-use clap::Arg;
 use log::LevelFilter;
 use simplelog::{ColorChoice, ConfigBuilder, TermLogger, TerminalMode};
 
 pub static LEVEL_FLAG_NAME: &str = "log_level";
-
-pub fn build_arg() -> Arg<'static, 'static> {
-    Arg::with_name(LEVEL_FLAG_NAME)
-        .short("-l")
-        .long("--log-level")
-        .env("LOG_LEVEL")
-        .help("Minimum level of logs to display")
-        .takes_value(true)
-        .possible_values(&["trace", "debug", "info", "warn", "error"])
-        .default_value("warn")
-}
 
 pub fn init_str(level: &str) {
     init(get_level(level));
@@ -23,7 +11,8 @@ pub fn init(level: LevelFilter) {
     let log_config = ConfigBuilder::new()
         .set_target_level(level)
         .set_max_level(LevelFilter::Info)
-        .set_time_to_local(true)
+        .set_time_offset_to_local()
+        .unwrap()
         .build();
 
     TermLogger::init(
